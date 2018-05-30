cfb68b78a86d2049560defbb8d5afac170db93dc
Nick Batsaras <batsarasnikos@gmail.com>
2018-05-30 23:24:44 +0300

    Add desktop setup

--- install.sh
+++ install.sh
@@ -131,3 +131,46 @@ useradd -m -G users,wheel -s /bin/bash "$username"
 echo "$password1" | passwd "$username" --stdin
 
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/locale.gen
+
+echo "
+
+  _____            _    _              
+ |  __ \          | |  | |             
+ | |  | | ___  ___| | _| |_ ___  _ __  
+ | |  | |/ _ \/ __| |/ / __/ _ \| '_ \ 
+ | |__| |  __/\__ \   <| || (_) | |_) |
+ |_____/ \___||___/_|\_\\__\___/| .__/ 
+  / ____|    | |                | |    
+ | (___   ___| |_ _   _ _ __    |_|    
+  \___ \ / _ \ __| | | | '_ \          
+  ____) |  __/ |_| |_| | |_) |         
+ |_____/ \___|\__|\__,_| .__/          
+                       | |             
+                       |_|             
+"
+
+pacman -S xorg-server xorg-xinit dialog wpa_supplicant
+pacman -S xf86-video-intel
+pacman -S xf86-input-synaptics
+
+echo "$username ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+su "$username"
+
+bash -c "$(wget https://raw.githubusercontent.com/nickbatsaras/dotfiles/master/X11/install.sh -O -)"
+bash -c "$(wget https://raw.githubusercontent.com/nickbatsaras/dotfiles/master/i3/install.sh -O -)"
+bash -c "$(wget https://raw.githubusercontent.com/nickbatsaras/dotfiles/master/bash/install.sh -O -)"
+bash -c "$(wget https://raw.githubusercontent.com/nickbatsaras/dotfiles/master/urxvt/install.sh -O -)"
+bash -c "$(wget https://raw.githubusercontent.com/nickbatsaras/dotfiles/master/tmux/install.sh -O -)"
+bash -c "$(wget https://raw.githubusercontent.com/nickbatsaras/dotfiles/master/vim/install.sh -O -)"
+bash -c "$(wget https://raw.githubusercontent.com/nickbatsaras/dotfiles/master/ranger/install.sh -O -)"
+bash -c "$(wget https://raw.githubusercontent.com/nickbatsaras/dotfiles/master/git/install.sh -O -)"
+
+sed -i "/$username ALL=(ALL) NOPASSWD: ALL/d" /etc/sudoers
+
+exit # as user to root
+exit # as root to live-media
+
+echo; echo
+echo "  Installation complete! You can now reboot ..."
+echo; echo
