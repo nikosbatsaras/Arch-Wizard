4d09d4644a8ba7f0d1c8d5b62eab2c85714ece71
Nick Batsaras <batsarasnikos@gmail.com>
2018-11-17 13:46:00 +0200

    Tweak mirrorlist & remove consolas font

--- extras.sh
+++ extras.sh
@@ -34,9 +34,7 @@ done
 sudo systemctl enable NetworkManager.service
 
 git clone https://aur.archlinux.org/ttf-ms-fonts.git
-git clone https://aur.archlinux.org/consolas-font.git
 
 cd ttf-ms-fonts  && makepkg -si --noconfirm && cd ..
-cd consolas-font && makepkg -si --noconfirm && cd ..
 
-rm -rf consolas-font/ ttf-ms-fonts/
+rm -rf ttf-ms-fonts/
