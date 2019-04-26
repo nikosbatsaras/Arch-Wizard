f7325c2cf6eef1f91828670706f1c53ba2ccf0e3
Nikos Batsaras <batsarasnikos@gmail.com>
2019-04-26 20:39:59 +0300

    Split bootstrap script

---
+++ mkfs.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+
+device="$1"
+boot_type="$2"
+table_type="$3"
+
+echo "  _____           _        _ _ _                        ";
+echo " |_   _|         | |      | | (_)                       ";
+echo "   | |  _ __  ___| |_ __ _| | |_ _ __   __ _    __ _    ";
+echo "   | | | '_ \/ __| __/ _\` | | | | '_ \ / _\` |  / _\` |";
+echo "  _| |_| | | \__ \ || (_| | | | | | | | (_| | | (_| |   ";
+echo " |_____|_| |_|___/\__\__,_|_|_|_|_| |_|\__, |  \__,_|   ";
+echo "  ______ _ _                     _      __/ |           ";
+echo " |  ____(_) |                   | |    |___/            ";
+echo " | |__   _| | ___  ___ _   _ ___| |_ ___ _ __ ___       ";
+echo " |  __| | | |/ _ \/ __| | | / __| __/ _ \ '_ \` _ \     ";
+echo " | |    | | |  __/\__ \ |_| \__ \ ||  __/ | | | | |     ";
+echo " |_|    |_|_|\___||___/\__, |___/\__\___|_| |_| |_|     ";
+echo "                        __/ |                           ";
+echo "                       |___/                            ";
+
+if [ "$boot_type" = "bios" ]
+then
+	if [ "$table_type" = "mbr" ]
+	then
+		if [[ "$device" =~ ^nvme.*$ ]]
+		then
+			mkfs.ext4 -F "/dev/${device}p1"
+			mkfs.ext4 -F "/dev/${device}p3"
+
+			mount "/dev/${device}p1" /mnt
+
+			mkdir /mnt/home
+
+			mount "/dev/${device}p3" /mnt/home
+		else
+			mkfs.ext4 -F "/dev/${device}1"
+			mkfs.ext4 -F "/dev/${device}3"
+
+			mount "/dev/${device}1" /mnt
+
+			mkdir /mnt/home
+
+			mount "/dev/${device}3" /mnt/home
+		fi
+	else
+		if [[ "$device" =~ ^nvme.*$ ]]
+		then
+			mkfs.ext4 -F "/dev/${device}p2"
+			mkfs.ext4 -F "/dev/${device}p4"
+
+			mount "/dev/${device}p2" /mnt
+
+			mkdir /mnt/home
+
+			mount "/dev/${device}p4" /mnt/home
+		else
+			mkfs.ext4 -F "/dev/${device}2"
+			mkfs.ext4 -F "/dev/${device}4"
+
+			mount "/dev/${device}2" /mnt
+
+			mkdir /mnt/home
+
+			mount "/dev/${device}4" /mnt/home
+		fi
+	fi
+else
+	if [[ "$device" =~ ^nvme.*$ ]]
+	then
+		mkfs.fat -F 32 "/dev/${device}p1"
+		mkfs.ext4 -F "/dev/${device}p2"
+		mkfs.ext4 -F "/dev/${device}p4"
+
+		mount "/dev/${device}p2" /mnt
+
+		mkdir /mnt/efi
+		mkdir /mnt/home
+
+		mount "/dev/${device}p1" /mnt/efi
+		mount "/dev/${device}p4" /mnt/home
+	else
+		mkfs.fat -F 32 "/dev/${device}1"
+		mkfs.ext4 -F "/dev/${device}2"
+		mkfs.ext4 -F "/dev/${device}4"
+
+		mount "/dev/${device}2" /mnt
+
+		mkdir /mnt/efi
+		mkdir /mnt/home
+
+		mount "/dev/${device}1" /mnt/efi
+		mount "/dev/${device}4" /mnt/home
+	fi
+fi
---
+++ partition.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+
+device="$1"
+boot_type="$2"
+table_type="$3"
+root_size="$4"
+home_size="$5"
+
+echo "  _____           _   _ _   _             _               ";
+echo " |  __ \         | | (_) | (_)           (_)              ";
+echo " | |__) |_ _ _ __| |_ _| |_ _  ___  _ __  _ _ __   __ _   ";
+echo " |  ___/ _\` | '__| __| | __| |/ _ \| '_ \| | '_ \ / _\` |";
+echo " | |  | (_| | |  | |_| | |_| | (_) | | | | | | | | (_| |  ";
+echo " |_|   \__,_|_|   \__|_|\__|_|\___/|_| |_|_|_| |_|\__, |  ";
+echo "  _____             _                              __/ |  ";
+echo " |  __ \           (_)                            |___/   ";
+echo " | |  | | _____   ___  ___ ___                            ";
+echo " | |  | |/ _ \ \ / / |/ __/ _ \                           ";
+echo " | |__| |  __/\ V /| | (_|  __/                           ";
+echo " |_____/ \___| \_/ |_|\___\___|                           ";
+echo "                                                          ";
+echo "                                                          ";
+
+if [ "$boot_type" = "bios" ]
+then
+	if [ "$table_type" = "mbr" ]
+	then
+		swap_size=$(echo "${root_size} + 1" | bc)
+		home_size=$(echo "${swap_size} + ${home_size}" | bc)
+
+		parted --script "/dev/${device}" mklabel msdos
+		parted --script "/dev/${device}" mkpart primary            "1MiB" "${root_size}GiB"
+		parted --script "/dev/${device}" mkpart primary "${root_size}GiB" "${swap_size}GiB"
+		parted --script "/dev/${device}" mkpart primary "${swap_size}GiB" "${home_size}GiB"
+
+		parted --script "/dev/${device}" set 1 boot on
+		parted --script "/dev/${device}" set 1 root on
+		parted --script "/dev/${device}" set 2 swap on
+
+		if [[ "$device" =~ ^nvme.*$ ]]; then
+			mkswap "/dev/${device}p2"
+			swapon "/dev/${device}p2"
+		else
+			mkswap "/dev/${device}2"
+			swapon "/dev/${device}2"
+		fi
+	else
+		swap_size=$(echo "${root_size} + 1" | bc)
+		home_size=$(echo "${swap_size} + ${home_size}" | bc)
+
+		parted --script "/dev/${device}" mklabel gpt
+		parted --script "/dev/${device}" mkpart primary            "1MiB"            "2MiB"
+		parted --script "/dev/${device}" mkpart primary            "2MiB" "${root_size}GiB"
+		parted --script "/dev/${device}" mkpart primary "${root_size}GiB" "${swap_size}GiB"
+		parted --script "/dev/${device}" mkpart primary "${swap_size}GiB" "${home_size}GiB"
+
+		parted --script "/dev/${device}" set 1 bios_grub on
+		parted --script "/dev/${device}" set 2 boot on
+		parted --script "/dev/${device}" set 2 root on
+		parted --script "/dev/${device}" set 3 swap on
+
+		if [[ "$device" =~ ^nvme.*$ ]]; then
+			mkswap "/dev/${device}p3"
+			swapon "/dev/${device}p3"
+		else
+			mkswap "/dev/${device}3"
+			swapon "/dev/${device}3"
+		fi
+	fi
+else
+	swap_size=$(echo "${root_size} + 1" | bc)
+	home_size=$(echo "${swap_size} + ${home_size}" | bc)
+
+	parted --script "/dev/${device}" mklabel gpt
+	parted --script "/dev/${device}" mkpart primary            "1MiB"            "1GiB"
+	parted --script "/dev/${device}" mkpart primary            "1GiB" "${root_size}GiB"
+	parted --script "/dev/${device}" mkpart primary "${root_size}GiB" "${swap_size}GiB"
+	parted --script "/dev/${device}" mkpart primary "${swap_size}GiB" "${home_size}GiB"
+
+	parted --script "/dev/${device}" set 2 boot on
+	parted --script "/dev/${device}" set 2 root on
+	parted --script "/dev/${device}" set 3 swap on
+
+	if [[ "$device" =~ ^nvme.*$ ]]; then
+		mkswap "/dev/${device}p3"
+		swapon "/dev/${device}p3"
+	else
+		mkswap "/dev/${device}3"
+		swapon "/dev/${device}3"
+	fi
+fi
+
+partprobe "/dev/$device"
